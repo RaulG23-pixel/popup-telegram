0526314d749b3400b718e44bf84185e09ab1e111
Raul Gonzalez <raulx12@gmail.com>
2025-07-16 11:58:46 -0400

    fix

--- popup.js
+++ popup.js
@@ -1,42 +1,11 @@
-window.onload = function(){
-    if(getCookie("popupWatched")){
-        return;
-    }
-    mostrarPopup();
-    
-    
-}
 
-const botonContinuar = document.querySelector(".popup__continuar");
-const botonSalir = document.querySelector(".popup__salir");
-const closeButton = document.querySelector(".close_button");
-
-
-closeButton.addEventListener("click", function(e) {
-    cerrarPopup();
-     // Set cookie to expire in 7 days
+let btnOcultar = document.querySelector(".btn_ocultar");
+let btnTelegram = document.querySelector(".btn_telegram button");
+btnTelegram.addEventListener("click", function() {
+    let popupContainer = document.querySelector("#telegramPopupOverlay");
+    popupContainer.style.display = "flex"; // Muestra el contenedor del popup
 });
-
-function mostrarPopup() {
-    document.querySelector(".popup_container").style.display = "flex";
-}
-
-function getCookie(name) {
-    return document.cookie.split('; ').reduce((r, v) => {
-        const parts = v.split('=');
-        return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-    }, '');
-}
-
-function setCookie(name, value, days) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
-}
-
-function ocultarPopup() {
-    document.querySelector(".popup_container").style.display = "none";
-}
-
-function cerrarPopup() {
-    document.querySelector(".popup_container.popup_container--telegram").style.display = "none";
-}
+btnOcultar.addEventListener("click", function() {
+    let popupContainer = document.querySelector("#telegramPopupOverlay");
+    popupContainer.style.display = "none"; // Oculta el contenedor del popup// Establece la cookie para no mostrar el popup nuevamente
+});
