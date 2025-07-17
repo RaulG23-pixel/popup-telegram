a232b2e8193d1a5647cefa71e83abade1b2f7644
Raul Gonzalez <raulx12@gmail.com>
2025-07-17 09:29:42 -0400

    fix

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 
 let btnOcultar = document.querySelector(".btn_ocultar");
 let btnTelegram = document.querySelector(".btn_telegram button");
+let btnCross = document.querySelector(".btn_cross button");
+btnCross.addEventListener("click", function() {
+    let popupContainer = document.querySelector("#telegramPopupOverlay");
+    popupContainer.style.display = "none"; // Oculta el contenedor del popup
+});
 btnTelegram.addEventListener("click", function() {
     let popupContainer = document.querySelector("#telegramPopupOverlay");
     popupContainer.style.display = "flex"; // Muestra el contenedor del popup
